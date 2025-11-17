cf867ef8f3c82e35bd5285e81fc1a613636c1605
Kuzmenko-Oleksandr <shparman17@gmail.com>
2025-11-17 14:46:53 +0200

    test commit for aws test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 
 // важно: слушаем порт из ENV, как любит AWS
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());           // чтобы Flutter Web тоже мог ходить
 app.use(express.json());   // чтобы читать JSON из body
