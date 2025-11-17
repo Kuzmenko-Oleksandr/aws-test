b639feebc2e7d42f33af69b44d2137480dcd196c
Kuzmenko-Oleksandr <shparman17@gmail.com>
2025-11-17 15:38:12 +0200

    test commit for aws test 3

--- server.js
+++ server.js
@@ -2,24 +2,42 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-
 const PORT = process.env.PORT || 8080;
+const API_KEY = process.env.API_KEY;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('API is working');
-});
-
+// health-check open
 app.get('/ping', (req, res) => {
     res.json({ status: 'ok' });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// 🔐 middleware
+function apiKeyMiddleware(req, res, next) {
+    // берём ключ из заголовка
+    const key = req.header('x-api-key');
+
+    if (!API_KEY) {
+        console.warn('API_KEY env is not set');
+        return res.status(500).json({ error: 'Server misconfigured' });
+    }
+
+    if (!key || key !== API_KEY) {
+        return res.status(403).json({ error: 'Forbidden' });
+    }
+
+    next();
+}
+
+
+app.use('/api', apiKeyMiddleware);
+
+
+app.get('/api/secret', (req, res) => {
+    res.json({ data: 'Secret data for Denis‍️' });
 });
 
-app.post('/test', (req, res) => {
-    res.json({ body: req.body });
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
